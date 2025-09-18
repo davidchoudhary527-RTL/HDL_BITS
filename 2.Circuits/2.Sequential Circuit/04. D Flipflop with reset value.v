b5951d446b4f5422a8420b58312ec16bee88f789
David Choudhary <davidchoudhary527@gmail.com>
2025-09-18 21:02:44 +0530

    Create 04. D Flipflop with reset value.v

---
+++ 2.Circuits/2.Sequential Circuit/04. D Flipflop with reset value.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input reset,
+    input [7:0] d,
+    output [7:0] q
+);
+
+    always @(negedge clk)
+        begin
+            q <= (!reset)? d : 8'h34;
+        end
+    
+endmodule
