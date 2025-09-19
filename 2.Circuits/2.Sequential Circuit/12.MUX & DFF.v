603b2b3f9d5b315ea7b779cd8154cec26e9a3989
David Choudhary <davidchoudhary527@gmail.com>
2025-09-19 20:05:44 +0530

    Create 12.MUX & DFF.v

---
+++ 2.Circuits/2.Sequential Circuit/12.MUX & DFF.v
@@ -0,0 +1,17 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+
+    always @(posedge clk)
+        begin
+            if(L)
+                Q <= R;
+            else
+                begin
+                    Q <= (E)? w : Q ;
+                end
+        end
+    
+endmodule
