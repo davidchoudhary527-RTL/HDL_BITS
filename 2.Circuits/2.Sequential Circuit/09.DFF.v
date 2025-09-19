12455f43a782f707edb61b877b99825d353619d8
David Choudhary <davidchoudhary527@gmail.com>
2025-09-19 19:59:09 +0530

    Create 09.DFF.v

---
+++ 2.Circuits/2.Sequential Circuit/09.DFF.v
@@ -0,0 +1,12 @@
+module top_module (
+    input clk,
+    input d, 
+    input r,   // synchronous reset
+    output q);
+
+    always @(posedge clk )
+        begin
+            q <= (r)? 0 : d ;
+        end
+    
+endmodule
