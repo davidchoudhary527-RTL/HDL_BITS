d7dee30f8f5328bc25f5254c4a812cbdb19a0967
David Choudhary <davidchoudhary527@gmail.com>
2025-09-21 01:24:39 +0530

    Create 16.Detect Any Edge.v

---
+++ 2.Circuits/2.Sequential Circuit/16.Detect Any Edge.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input [7:0] in,
+    output [7:0] anyedge
+);
+    reg [7:0] in_d1;
+    always @(posedge clk)
+        begin
+            in_d1 <= in ;
+            anyedge <= in ^ in_d1;
+        end
+
+endmodule
