fdebc03eb02171f4fff32c12b68d273146ca8b7c
David Choudhary <davidchoudhary527@gmail.com>
2025-10-09 22:29:14 +0530

    Create 03.Simple FSM 2 ASYNC.v

---
+++ 2.Circuits/2.Sequential Logic/5.Finite State Machine/03.Simple FSM 2 ASYNC.v
@@ -0,0 +1,30 @@
+module top_module(
+    input clk,
+    input areset,    // Asynchronous reset to OFF
+    input j,
+    input k,
+    output out); //  
+
+    parameter OFF=0, ON=1; 
+    reg state, next_state;
+
+    always @(*) begin
+        state <= next_state;
+    end
+
+    always @(posedge clk, posedge areset) begin
+        if(areset)
+            next_state <= OFF;
+        else
+            begin
+                case(state)
+                    ON : next_state <= (k)? OFF : ON;
+                    OFF: next_state <= (j)? ON  : OFF;
+                endcase
+            end
+    end
+
+    // Output logic
+                    assign out = (state == ON);
+
+endmodule
