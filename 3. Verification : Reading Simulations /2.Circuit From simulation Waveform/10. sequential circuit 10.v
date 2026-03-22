2131d3bc52764ebaf88cd3ecdcb68508d51b2e70
David Choudhary <davidchoudhary527@gmail.com>
2026-03-22 20:02:36 +0530

    Create 10. sequential circuit 10.v

---
+++ 3. Verification : Reading Simulations /2.Circuit From simulation Waveform/10. sequential circuit 10.v
@@ -0,0 +1,13 @@
+module top_module(
+    input clk,
+    input a,
+    output [3:0] q );
+
+    always @(posedge clk)
+        begin
+            if(a)
+                q <=4 ;
+            else
+                q <= (q < 6)? q+1 : 'h0 ;
+        end
+endmodule
